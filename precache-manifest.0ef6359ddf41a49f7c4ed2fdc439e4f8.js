f044b06c11f9de7d039982beeec06b0d4632be66
zsevic <zsevic95@gmail.com>
2019-02-24 13:58:37 +0100

    Updates

---
+++ precache-manifest.0ef6359ddf41a49f7c4ed2fdc439e4f8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "68a69234833efa9c4122",
+    "url": "/upis/static/js/runtime~main.68a69234.js"
+  },
+  {
+    "revision": "f6980f7c846db8df8192",
+    "url": "/upis/static/js/main.f6980f7c.chunk.js"
+  },
+  {
+    "revision": "4dfd211cf206977c45c9",
+    "url": "/upis/static/js/2.4dfd211c.chunk.js"
+  },
+  {
+    "revision": "4dfd211cf206977c45c9",
+    "url": "/upis/static/css/2.348216ec.chunk.css"
+  },
+  {
+    "revision": "da2c78cdd8c7c7a0e7f757f77abf2ea2",
+    "url": "/upis/index.html"
+  }
+];
