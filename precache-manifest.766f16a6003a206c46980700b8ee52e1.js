5dd6f16ebc0786d2cf8ede9b9803a9e4b3c36cf6
zsevic <zsevic95@gmail.com>
2020-12-05 16:16:53 +0100

    Updates

---
+++ precache-manifest.766f16a6003a206c46980700b8ee52e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a06ad92e12f6045a8b0d",
+    "url": "/upis/static/js/main.b7d18139.chunk.js"
+  },
+  {
+    "revision": "68a69234833efa9c4122",
+    "url": "/upis/static/js/runtime~main.9338df7d.js"
+  },
+  {
+    "revision": "db99c5bee7a5b4988299",
+    "url": "/upis/static/css/2.85fbc684.chunk.css"
+  },
+  {
+    "revision": "db99c5bee7a5b4988299",
+    "url": "/upis/static/js/2.11893072.chunk.js"
+  },
+  {
+    "revision": "3b2f0cdeeb0969bc3ca17b6a896eacae",
+    "url": "/upis/static/media/logo.3b2f0cde.svg"
+  },
+  {
+    "revision": "e092eea2a4b9dc4d4bd5680b817246c3",
+    "url": "/upis/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -11,19 +11,13 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/upis/precache-manifest.2d4e82954097d3fbe4d83fb6e38071d0.js"
+  "/upis/precache-manifest.766f16a6003a206c46980700b8ee52e1.js"
 );
 
-self.addEventListener('message', (event) => {
-  if (event.data && event.data.type === 'SKIP_WAITING') {
-    self.skipWaiting();
-  }
-});
-
-workbox.core.clientsClaim();
+workbox.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -31,9 +25,10 @@ workbox.core.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/upis/index.html"), {
+workbox.routing.registerNavigationRoute("/upis/index.html", {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
